e92d3da0a43f0aeb1d7d8930279b179258494a86
rakshitha HR <rakshithahr39@gmail.com>
2023-02-27 12:25:52 +0530

    uppdated

---
+++ myscript.js
@@ -0,0 +1,13 @@
+function myfun(){
+    document.getElementById('demo').style.display='none';
+}
+
+
+fuction myfun2(){
+    document.getaelementById('demo').style.fontsize='50px';
+}
+
+
+function myfun3(){
+    document.getElementById("demo").innerHTML=5+6;
+}
